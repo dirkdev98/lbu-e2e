92f4e88c527adf36a04a9b58c53ba5434b65f098
Dirk de Visser <dirkdev98@gmail.com>
2020-03-30 19:51:33 +0200

    Listen to PORT instead of API_PORT

--- src/api.js
+++ src/api.js
@@ -10,7 +10,7 @@ mainFn(import.meta, log, main);
 async function main(logger) {
   const app = constructApp();
 
-  const port = process.env.API_PORT || 3000;
+  const port = process.env.PORT || 3000;
   app.listen(port, () => {
     logger.info("Listening", { port });
   });
